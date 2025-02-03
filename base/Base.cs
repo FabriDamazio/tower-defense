40d629e9f87f4b29d7ad485e2fe0404450b448ed
FabriDamazio <fabridamazio@gmail.com>
2025-02-03 11:11:39 -0300

    Create the base and setup initial collision with enemies

---
+++ base/Base.cs
@@ -0,0 +1,9 @@
+using Godot;
+
+public partial class Base : Node3D
+{
+  public void TakeDamage()
+  {
+    GD.Print("Damage dealt to base");
+  }
+}
--- enemy/Enemy.cs
+++ enemy/Enemy.cs
@@ -2,15 +2,23 @@ using Godot;
 
 public partial class Enemy : PathFollow3D
 {
-  [Export]
-  public float Speed = 2.5f;
+    [Export]
+    public float Speed = 2.5f;
 
-	public override void _Ready()
-	{
-	}
+    private Base _base;
 
-	public override void _Process(double delta)
-	{
-    Progress += (float)delta * Speed;
-	}
+    public override void _Ready()
+    {
+        _base = GetTree().GetFirstNodeInGroup("base") as Base;
+    }
+
+    public override void _Process(double delta)
+    {
+        Progress += (float)delta * Speed;
+
+        if (ProgressRatio == 1.0)
+        {
+            _base.TakeDamage();
+        }
+    }
 }
