759c5503b7d011bb2b9d5589f8c0d8c7d5107c36
FabriDamazio <fabridamazio@gmail.com>
2025-02-04 11:46:06 -0300

    Updating gridmap cells when hovering

--- ray_picker_camera/RayPickerCamera.cs
+++ ray_picker_camera/RayPickerCamera.cs
@@ -5,6 +5,9 @@ public partial class RayPickerCamera : Camera3D
     [Export]
     public float ClickableRange = 100.0f;
 
+    [Export]
+    public GridMap GridMap;
+
     private RayCast3D _rayCast3D;
 
     public override void _Ready()
@@ -17,7 +20,21 @@ public partial class RayPickerCamera : Camera3D
         Vector2 mousePosition = GetViewport().GetMousePosition();
         _rayCast3D.TargetPosition = ProjectLocalRayNormal(mousePosition) * ClickableRange;
         _rayCast3D.ForceRaycastUpdate();
-        GD.Print(_rayCast3D.GetCollider());
-        GD.Print(_rayCast3D.GetCollisionPoint());
+
+        if (_rayCast3D.IsColliding())
+        {
+            var collider = _rayCast3D.GetCollider();
+
+            if (collider is GridMap)
+            {
+                Vector3 collisionPoint = _rayCast3D.GetCollisionPoint();
+                Vector3I cellPosition = GridMap.LocalToMap(collisionPoint);
+
+                if (GridMap.GetCellItem(cellPosition) == 0)
+                {
+                    GridMap.SetCellItem(cellPosition, 1);
+                }
+            }
+        }
     }
 }
