d4bd0b6fab2bb2315d795a9a33a45811ce22ca5f
FabriDamazio <fabridamazio@gmail.com>
2025-02-07 13:49:10 -0300

    Turret range and find the best target logic

--- turret/Turret.cs
+++ turret/Turret.cs
@@ -1,14 +1,17 @@
 using Godot;
-using System.Linq;
 
 public partial class Turret : Node3D
 {
     [Export]
     public PackedScene Projectile;
 
+    [Export]
+    public float TurretRange = 10.0f;
+
     private Timer _shotTimer;
     private Node3D _turretTop;
     private Path3D _enemyPath;
+    private PathFollow3D _target;
 
     public override void _Ready()
     {
@@ -20,8 +23,10 @@ public partial class Turret : Node3D
 
     public override void _PhysicsProcess(double delta)
     {
-        var enemy = _enemyPath.GetChildren().Last() as Enemy;
-        LookAt(enemy.GlobalPosition, Vector3.Up, true);
+        _target = FindBestTarget();
+
+        if (_target is not null)
+            LookAt(_target.GlobalPosition, Vector3.Up, true);
     }
 
     public void SetEnemyPath(Path3D path)
@@ -31,9 +36,35 @@ public partial class Turret : Node3D
 
     private void OnTimerTimeout()
     {
-        var shot = Projectile.Instantiate<Projectile>();
-        AddChild(shot);
-        shot.GlobalPosition = _turretTop.GlobalPosition;
-        shot.Direction = GlobalBasis.Z;
+        if (_target is not null)
+        {
+            var shot = Projectile.Instantiate<Projectile>();
+            AddChild(shot);
+            shot.GlobalPosition = _turretTop.GlobalPosition;
+            shot.Direction = GlobalBasis.Z;
+        }
+    }
+
+    private PathFollow3D FindBestTarget()
+    {
+        PathFollow3D bestTarget = null;
+        float bestProgress = 0;
+
+        foreach (var enemy in _enemyPath.GetChildren())
+        {
+            if (enemy is PathFollow3D)
+            {
+                var path = (PathFollow3D)enemy;
+                var distance = GlobalPosition.DistanceTo(path.GlobalPosition);
+
+                if (distance <= TurretRange && path.Progress > bestProgress)
+                {
+                    bestTarget = path;
+                    bestProgress = path.Progress;
+                }
+            }
+        }
+
+        return bestTarget;
     }
 }
